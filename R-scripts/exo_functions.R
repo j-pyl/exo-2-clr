43d4b9d27ecf75f92305ac4ef32b87d416539978
j-pyl <133907776+j-pyl@users.noreply.github.com>
2024-09-04 19:22:25 +0100

    Add xy to extract_peaks
    Added code necessary to read all CoordData, transpose them, and add them all to a df.
    
    Added merge to extract_peak_xyt to include xy data for each peak.

--- R-scripts/exo_functions.R
+++ R-scripts/exo_functions.R
@@ -198,10 +198,11 @@ average_waves <- function(df,s) {
 #' 
 #' @param indf Input dataframe. What will be used to find filename and extract peak coordinates.
 #' @param frecy File recycle. Input '0t' or '1t'. String to specify whether the current data is from recy-0 or recy-1.
+#' @param xydf Dataframe containing all xy coordinates (Coorddf).
 #' 
 #' @return Create .csv files for each dish with xy and frame coordinates of each peak detected in with findpeaks
 
-extract_peak_xyt <- function(indf,frecy) {
+extract_peak_xyt <- function(indf,frecy,xydf) {
   img <- paste0("img_",frecy)
   knd <- paste0("kind_",frecy)
   nm <- paste0("name_",frecy)
@@ -210,16 +211,19 @@ extract_peak_xyt <- function(indf,frecy) {
   allpeaks_df <- indf %>% 
     filter(NewFrame == 0) %>% 
     select(img,knd,'frame',nm) %>%
-    rename(spot_no = nm) # will this work? copilot chat suggested the below
-    # rename(spot_no = !!sym(nm))
+    rename(spot_no = nm)
   allpeaks_df$condname <- paste0(allpeaks_df[[img]],"_",allpeaks_df[[knd]])
-  # Save all peaks into df
-  write.csv(allpeaks_df, "Output/Data/peak-xyt/all-peaks-xyt.csv", row.names = FALSE)
+  
+  # Merge in xy coordinates
+  xyt_df <- merge(allpeaks_df, xydf, by = c("condname","spot_no"))
+  
+  # Save all peaks into a single df
+  write.csv(xyt_df, paste0("Output/Data/peak-xyt/all-peaks-xyt_",frecy,".csv"), row.names = FALSE)
   
   # Save new file with detected spot for each dish
-  uniqueCond <- unique(allpeaks_df$condname)
-  for (obj in uniqueCond) { # could simplify with group_by()?
-    temp <- allpeaks_df %>% 
+  uniqueCond <- unique(xyt_df$condname)
+  for (obj in uniqueCond) {
+    temp <- xyt_df %>% 
       filter(condname == obj)
     write.csv(temp, paste0("Output/Data/peak-xyt/",obj,"_peak-xyt.csv"), row.names = FALSE)
   }
